2bebcf252ab0e81d9e0ac9e24c0f26349f1d7f78
SamarthKadam <samarthskadam14@gmail.com>
2024-06-25 10:48:15 +0530

    some changes

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useId } from 'react'
+import { auth, currentUser } from "@clerk/nextjs/server";
 
-export default function page() {
+export default async function page() {
+
+  const { userId } = auth();
+  console.log(useId)
+  const user = await currentUser()
+  console.log(user);
+
   return (
     <div>page</div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+'use client'
 import Image from "next/image";
 import { GridBackgroundDemo } from "./components/ui/GridBackground";
 import GlobeDemo from "./components/GlobeDemo";
 import {Noto_Sans} from 'next/font/google'
 import { SignUp } from "@clerk/nextjs";
+import { ButtonsCard } from '@/app/components/ui/tailwindcss-buttons'
 
 const notoSans = Noto_Sans({ subsets: ['latin'] });
 
@@ -12,12 +14,14 @@ export default function Home() {
         <div className="flex flex-col justify-center ">
         <div className={`${notoSans.className} text-8xl font-medium tracking-tighter text-[#9ED0E6] `}>CollaborFlow</div>
         <div className="text-white ml-4 my-2">Where real-time creativity meets seamless collaboration.</div>
+        <ButtonsCard onClick={()=>{console.log("clicked")}}>
         <button className="relative  inline-flex mt-8 ml-4 h-12 w-36 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
         <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
         <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
           Start Now
         </span>
       </button>
+        </ButtonsCard>
         </div>
       <GlobeDemo></GlobeDemo>
       </div>
